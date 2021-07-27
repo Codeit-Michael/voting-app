5c3cae755d01013618cc9cf78e1485a9c189a087
Michael Maranan <michaelmaranan030@gmail.com>
2021-07-27 18:01:53 +0800

    adding views(UI)

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import question,choice
 
 # Register your models here.
+admin.site.register(question)
+admin.site.register(choice)
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import question,choice
+from django.http import Http404
 
 # Create your views here.
-def func1(request):
-    return HttpResponse('hello world')
+def index(request):
+    latest_question_list = question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    try:
+        myquestion = question.objects.get(pk=question_id)
+    except question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': myquestion})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
