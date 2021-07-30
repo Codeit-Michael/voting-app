500f185110a11fa0dba082608bce726a3e5783d1
Michael Maranan <michaelmaranan030@gmail.com>
2021-07-30 23:28:20 +0800

    custom admin site

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,36 @@
 from django.contrib import admin
-from .models import question,choice
 
-# Register your models here.
-admin.site.register(question)
-admin.site.register(choice)
+# must import these all
+from .models import choice, question,was_published_recently
+
+# stacks
+# class ChoiceInline(admin.StackedInline):
+#     model = choice
+#     extra = 3
+
+# the same as the above, difference is its in the same line, unlike the 1st 1, stacked
+class ChoiceInline(admin.TabularInline):
+	model = choice
+	extra = 3
+
+# Question format
+class QuestionAdmin(admin.ModelAdmin):
+	# The Date Information can hide/show in admin site because of the ['collapse']
+	fieldsets = [
+		(None,               {'fields': ['question_text']}),
+		('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+	]
+
+	# shows and allows you to add choices while doing the question
+	inlines = [ChoiceInline]
+
+	# To view each question w/ its details after clicking the 'Questions' in sidebar
+	list_display = ('question_text', 'pub_date', was_published_recently)
+
+	# for filter sidebar via pub_date
+	list_filter = ['pub_date']
+
+	# allows you to search
+	search_fields = ['question_text']
+
+admin.site.register(question, QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -3,6 +3,8 @@ import datetime
 from django.db import models
 from django.utils import timezone
 
+from django.contrib import admin
+
 # Create your models here.
 
 class question(models.Model):
@@ -11,7 +13,14 @@ class question(models.Model):
 
     def __str__(self):
         return self.question_text
-
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class choice(models.Model):
     question = models.ForeignKey(question, on_delete=models.CASCADE)
